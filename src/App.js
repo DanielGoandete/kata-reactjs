58dd5fb5dd57c7466d00ed55b125f52a2b9c1047
DanielGoandete <63291047+DanielGoandete@users.noreply.github.com>
2021-11-18 15:08:13 +0100

    books details
    all information of the book that i will need after

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import Header from "./components/Header";
 import Image from "./components/Image";
 import './App.css';
-import cleanCode from '../src/assets/images/cleanCode.png'
-import cleanCoder from '../src/assets/images/cleanCoder.png'
-import cleanArchitecture from '../src/assets/images/cleanArchitecture.jpeg'
-import tdd from '../src/assets/images/tdd.jpeg'
-import refactoring from '../src/assets/images/refactoring.jpeg'
+
+
+
 
 function App() {
+
+
   return (
     <div className="App">
         <div className="container">
           <Header></Header>
-          <div class="row">
-              <Image pathName={cleanCode} altText="A HandBook of Agile Software Craftmanship"></Image>
-              <Image pathName={cleanCoder} altText="A Code of Conduct Professional Programmers"></Image>
-              <Image pathName={cleanArchitecture} altText="A Craftman's Guide to Software Structure and Design"></Image>
-              <Image pathName={tdd} altText="Test-Driven Development"></Image>
-              <Image pathName={refactoring} altText="Working Effectively"></Image>
-          </div>
+          <Image></Image>
         </div>
     </div>
   );
 }
-
 export default App;
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,35 @@
+import { useState } from 'react'
+import cleanCode from '../../src/assets/images/cleanCode.png'
+import cleanCoder from '../../src/assets/images/cleanCoder.png'
+import cleanArchitecture from '../../src/assets/images/cleanArchitecture.jpeg'
+import tdd from '../../src/assets/images/tdd.jpeg'
+import refactoring from '../../src/assets/images/refactoring.jpeg'
 
+const Image = () => {
 
-const Image = ({pathName, altText}) => {
+    let booksData = [
+        {id:0, price:50, imgSrc:cleanCode, alt:"A HandBook of Agile Software Craftmanship"},
+        {id:1, price:50, imgSrc:cleanCoder,alt:"A Code of Conduct Professional Programmers"},
+        {id:2, price:50, imgSrc:cleanArchitecture,alt:"A Craftman's Guide to Software Structure and Design"},
+        {id:3, price:50, imgSrc:tdd,alt:"Test-Driven Development"},
+        {id:4, price:50, imgSrc:refactoring,alt:"Working Effectively"}
+    ];
+  
+    const getPrice =(id)=>{
+        console.warn(id)
+    }
+
     return (
-        <div class="col">
-            <img src={pathName} alt={altText}  width="90%" height="100%" />        
+        <div className="row">
+           {
+            booksData.map((bookDetail,index)=>{
+                return(
+                    <div className="col" key={index}>
+                      <img src={bookDetail.imgSrc} alt={bookDetail.alt}   width="90%" height="100%" />  
+                      <button className='btn btn-success buttonBuy' onClick={()=> getPrice(bookDetail.id)}>Buy</button>     
+                    </div>
+                )
+           })}
         </div>
     )
 }
